295bdd1b2b72e9ceeb65d4374f90d57c507e0a9a
Seif <seifahmedjr7@gmail.com>
2025-12-31 19:01:03 +0200

    Add initial project files including HTML, CSS, and JavaScript

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import { getProducts } from "./product";
+let data = getProducts();
+console.log(data);
---
+++ js/product.js
@@ -0,0 +1,4 @@
+export async function getProducts() {
+  let response = await fetch("https://fakestoreapi.com/products");
+  return response.json();
+}
